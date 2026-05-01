ed4d083031cd325654ae3a58e81365657d71fe88
Shanzay <malikshanzay135@gmail.com>
2026-05-01 16:03:14 +0500

    Update TIC-TAC-TOE.h

--- TIC-TAC-TOE/TIC-TAC-TOE.h
+++ TIC-TAC-TOE/TIC-TAC-TOE.h
@@ -5,16 +5,21 @@
 #include<string>
 using namespace std;
 using Board = vector<vector<string>>;
-void ticTacToe();
-void printBoard( const Board& b, int n);
-void formatBoard(Board& b, int n);
-bool isWinner(const Board& b, int n);
-bool checkimgVerticalX(const Board& b, int n);
-bool checkimgHorizontalX(const Board& b, int n);
-bool checkimgVerticalO(const Board& b, int n);
-bool checkimgHorizontalO(const Board& b, int n);
-bool checkingDiagonalO(const Board& b, int n);
-bool checkingDiagonalX(const Board& b, int n);
-bool checkingCounterDiagonalO(const Board& b, int n);
-bool checkingCounterDiagonalX(const Board& b, int n);
+class TicTacToe
+{
+      void printBoard( const Board& b, int n);
+      void formatBoard(Board& b, int n);
+      bool isWinner(const Board& b, int n);
+      bool checkimgVerticalX(const Board& b, int n);
+      bool checkimgHorizontalX(const Board& b, int n);
+      bool checkimgVerticalO(const Board& b, int n);
+      bool checkimgHorizontalO(const Board& b, int n);
+      bool checkingDiagonalO(const Board& b, int n);
+      bool checkingDiagonalX(const Board& b, int n);
+      bool checkingCounterDiagonalO(const Board& b, int n);
+      bool checkingCounterDiagonalX(const Board& b, int n);
+public:
+      TicTacToe();
+      void play();
+};
 #endif
